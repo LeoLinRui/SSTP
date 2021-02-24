0ea16a88f684c345b6317791e74eae3eed9d0945
MinhTheMerciless <64875104+MinhTheMerciless@users.noreply.github.com>
2021-02-24 05:06:24 -0800

    Create frame_extractor.py

---
+++ GruGan/frame_extractor.py
@@ -0,0 +1,27 @@
+import cv2.Videocapture
+import cv2.imwrite
+import os
+import matplotlib.pyplot as plt
+
+def extract_frames(video: str, outdir=None):
+  vidcap = cv2.VideoCapture(video)
+  imgs = []
+  success = True
+
+  while success:
+    success,img = vidcap.read()
+    imgs.append(img)
+  
+  imgs.pop(-1)
+
+  if outdir is not None:
+    os.mkdir(outdir)
+    num_imgs = len(imgs)
+
+    for idx, img in enumerate(imgs):
+      path = os.path.join(outdir,f"frame{idx:05d}.jpg")
+      cv2.imwrite(path, img)
+      print(f'Saved {path}...Progress: {idx}/{num_imgs}')
+
+  print("Done!")
+  return imgs
