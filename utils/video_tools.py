36d3b8b3a52890fd3c72f02ed47443cee1246bc9
MinhTheMerciless <64875104+MinhTheMerciless@users.noreply.github.com>
2021-03-09 06:09:05 -0800

    Added B&W support and documentation to numpy2video()

--- utils/video_tools.py
+++ utils/video_tools.py
@@ -25,7 +25,6 @@ def gif_from_folder(imgs_path: str, gif_path='gif/gif.gif', fps=30):
 
 #----------------------------------------------------------------------------
 
-#TODO: This shit sometimes works
 def mp4_from_folder(imgs_path: str, mp4_path='video.mp4', fps=30):
     assert imgs_path.endswith('/*.png')
     assert mp4_path.endswith('.mp4')
@@ -46,27 +45,34 @@ def mp4_from_folder(imgs_path: str, mp4_path='video.mp4', fps=30):
 #----------------------------------------------------------------------------
 
 def numpy2video(
-    arr: np.ndarray, 
-    mp4_path="np_out.mp4", 
-    fps=30, 
-    scale=True, 
-    target_dim=(1920,1080)
+    arr: np.ndarray, # array of cv2 images
+    mp4_path="np_out.mp4", # path to output mp4 video
+    fps=30, # framerate
+    scale=True, # whether to scale from 0-1 to 0-255
+    target_dim=(1920,1080), # tuple of (width, height) for the target video size
+    is_color=True # whether the input images are color
     ):
+  
+  """
+  Creates video from aray of cv2-format images
+
+  """
   
   assert mp4_path.endswith('.mp4')
   
   frameSize= (target_dim[0], target_dim[1])
-  out = VideoWriter(mp4_path,VideoWriter_fourcc(*'MP4V'), fps, frameSize)
+  out = VideoWriter(
+      mp4_path,VideoWriter_fourcc(*'MP4V'), fps, frameSize, is_color)
     
   if scale:
     for img in arr:
       img = np.multiply(img, 255).astype("uint8")
-      img = resize(img, target_dim, interpolation = INTER_CUBIC)
+      img = resize(img, target_dim, interpolation=INTER_CUBIC)
       out.write(img)
   else:
     for img in arr:
       img = img.astype("uint8")
-      img = resize(img, target_dim, interpolation = INTER_CUBIC)
+      img = resize(img, target_dim, interpolation=INTER_CUBIC)
       out.write(img)
 
   out.release()
